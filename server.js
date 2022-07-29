083fb045642b976504eebddc0b6fd2dda0a61bf7
amirkhan_web <amirhanvalman@gmail.com>
2022-07-29 20:52:10 +0300

    add auth-json-server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('./server/db.json');
 const middlewares = jsonServer.defaults({
@@ -11,7 +12,17 @@ server.use(
       '/api/*': '/$1',
    })
 );
+
+const rules = auth.rewriter({
+   // Permission rules
+   users: 600,
+   messages: 640,
+   // Other rules
+});
+
+server.use(rules);
 server.use(router);
+server.use(auth);
 server.listen(PORT, () => {
    console.log('Server is running');
 });
