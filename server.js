d7e66d9c67a33e9121ba9b7242661bf5cf4f5861
amirkhan_web <amirhanvalman@gmail.com>
2022-07-29 21:03:15 +0300

    init-server.js

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const middlewares = jsonServer.defaults({
    static: './build',
 });
 
-server.db = router.db;
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 
 server.use(
    jsonServer.rewriter({
       '/api/*': '/$1',
+   }),
+   auth.rewriter({
+      '/api/*': '/$1',
    })
 );
 
 server.use(router);
 server.use(auth);
-
-const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
    console.log('Server is running');
