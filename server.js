e3a276aefbd5a3337f335500dcf9f28b25e445c7
amirkhan_web <amirhanvalman@gmail.com>
2022-07-29 20:58:45 +0300

    rsss

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+
 const server = jsonServer.create();
 const router = jsonServer.router('./server/db.json');
+
 const middlewares = jsonServer.defaults({
    static: './build',
 });
-const PORT = process.env.PORT || 3001;
+
+server.db = router.db;
+
 server.use(middlewares);
+
 server.use(
    jsonServer.rewriter({
       '/api/*': '/$1',
    })
 );
 
-const rules = auth.rewriter({
-   // Permission rules
-   users: 600,
-   messages: 640,
-   // Other rules
-});
-
-server.use(rules);
 server.use(router);
 server.use(auth);
+
+const PORT = process.env.PORT || 3001;
+
 server.listen(PORT, () => {
    console.log('Server is running');
 });
